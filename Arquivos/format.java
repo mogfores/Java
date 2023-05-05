18cd455692d86cdd43c06ba4c0f1411f5d0db797
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-05 17:15:37 -0300

    Update and rename Main.java to format.java

--- Arquivos/format.java
+++ Arquivos/format.java
@@ -5,27 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//int y = 32;
-		//double x = 10.35784;
-		
-		//System.out.println(y);
-		
-		//System.out.printf("%.2f%n", x);
-		//System.out.printf("%.4f%n", x);
-		//Locale.setDefault(Locale.US);
-		//System.out.printf("%.4f%n", x);
-		
-		//System.out.println("Resultado = " + x + " metros");
-		
-		
-		//System.out.printf("Resultado = %.2f metros%n", x);
-		
-		//String nome = "Eduardo";
-		//int idade = 36;
-		//double renda = 4000.0;
-		//System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
-		
-		
+				
 		String product1 = "Computer";
 		String product2 = "Office desk";
 		
