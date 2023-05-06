9c73e034f9be754f1850954b8e7eace6beeea27f
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-05 21:52:43 -0300

    Add files via upload

---
+++ Arquivos/Main.java
@@ -0,0 +1,86 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+	//PARA LER UMA PALAVRA A PARTIR DO TECLADO
+		
+	//Scanner sc = new Scanner(System.in);
+	
+	//String x;
+	//x = sc.next();
+	//System.out.println("voce digitou: " + x);
+		
+	//sc.close();
+	
+	//PARA LER UM NUMERO INTEIRO
+	
+    //Scanner sc = new Scanner(System.in);
+	
+	//int x;
+	//x = sc.nextInt();
+	//System.out.println("voce digitou: " + x);
+		
+	//sc.close();
+	
+	//PARA LER UM NUMERO COM PONTO FLUTUANTE
+	
+	//Scanner sc = new Scanner(System.in);
+		
+	//double x;
+	//x = sc.nextDouble();
+	//System.out.println("voce digitou: " + x);
+			
+	//sc.close();
+		
+	//PARA LER UM CARACTERE
+		
+	//Scanner sc = new Scanner(System.in);
+		
+	//char x;
+	//x = sc.next().charAt(0);
+	//System.out.println("Voce digitou: " + x);
+	
+	//sc.close();
+		
+	//PARA LER VARIOS DADOS NA MESMA LINHA
+	
+	//Scanner sc = new Scanner(System.in);
+		
+	//String x;
+	//int y;
+	//double z;
+	
+	//x = sc.next();
+	//y = sc.nextInt();
+	//z = sc.nextDouble();
+	//System.out.println("Dados Digitados");
+	//System.out.println(x);
+	//System.out.println(y);
+	//System.out.println(z);
+	
+	//sc.close();
+		
+	// PARA LER UM TEXTO ATÉ A QUEBRA DE LINHA
+	
+	//Scanner sc = new Scanner(System.in);
+	
+	//String s1, s2, s3;
+	
+	//s1 = sc.nextLine();
+	//s2 = sc.nextLine();
+	//s3 = sc.nextLine();
+	
+	//System.out.println("DADOS DIGITADOS:");
+	//System.out.println(s1);
+	//System.out.println(s2);	
+	//System.out.println(s3);
+	
+	//sc.close();
+	}
+
+}
