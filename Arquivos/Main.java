451eac71f4ca5d9d5eec1a799654a7a95a922ad6
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-06 20:23:30 -0300

    Add files via upload

---
+++ Arquivos/Main.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double valor;
+		
+		valor = sc.nextDouble();
+		
+		
+		if (valor >= 0 && valor <= 25.00) {
+			System.out.printf("Intervalo [0,25]");
+		}
+		else {
+			if (valor > 25 && valor <= 50) {
+				System.out.printf("Intervalo (25, 50]");
+			}
+			else {
+				if (valor > 50 && valor <= 75) {
+					System.out.printf("Intervalo (50, 75]");
+				    }
+					else {
+						if (valor > 75 && valor <= 100) {
+							System.out.printf("Intervalo (75, 100]");
+						    }
+							else {
+								System.out.printf("Fora de Intervalo");
+							}
+						 sc.close();
+					    }							
+					}
+				}
+			}
+        }
+
+        
