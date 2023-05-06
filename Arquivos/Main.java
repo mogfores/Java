a8ad013e8f35f1aefc6b4f8000677d01688b9c10
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-06 16:07:42 -0300

    Add files via upload

---
+++ Arquivos/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		
+		x = sc.nextInt();
+		
+		if (x % 2 == 0) {
+			System.out.println("PAR");
+		}
+		else {
+			System.out.println("IMPAR");
+		}	
+	
+		sc.close();
+	}
+
+}
