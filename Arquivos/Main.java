1fae611f16e82ebc120bbd219eba0646ec5fb373
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-06 15:46:50 -0300

    Add files via upload

---
+++ Arquivos/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		
+		x = sc.nextInt();
+		
+		if (x < 0) {
+			System.out.println("NEGATIVO");
+		}
+		else {
+			System.out.println("NAO NEGATIVO");
+		}	
+	
+		sc.close();
+	}
+
+}
