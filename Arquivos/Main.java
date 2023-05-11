c165d50341fdcad93a2d0e736a8250098958ee0c
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-11 17:16:35 -0300

    Add files via upload

---
+++ Arquivos/Main.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		Double x, y;
+
+		x = sc.nextDouble();
+		y = sc.nextDouble();
+
+		while (x != 0 && y != 0) {
+
+			if (x > 0 && y > 0) {
+				System.out.println("primeiro");
+			} else {
+				if (x < 0 && y > 0) {
+					System.out.println("segundo");
+				} else {
+					if (x < 0 && y < 0) {
+						System.out.println("terceiro");
+					} else {
+						System.out.println("quarto");
+					}
+
+				}
+			}
+			x = sc.nextDouble();
+			y = sc.nextDouble();
+
+		}
+		sc.close();
+	}
+}
