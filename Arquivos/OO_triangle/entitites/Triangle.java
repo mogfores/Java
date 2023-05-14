f4477da5ef27b5b1476e8d106ad92b68a90bf73c
Eduardo Mogfores <89140035+mogfores@users.noreply.github.com>
2023-05-14 14:25:41 -0300

    Update Triangle.java

--- Arquivos/OO_triangle/entitites/Triangle.java
+++ Arquivos/OO_triangle/entitites/Triangle.java
@@ -5,4 +5,10 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
+
+	// Função para se calcular a area
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
